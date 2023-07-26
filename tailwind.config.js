9057925f9489b791929ea433b011de927f549406
XSam28 <petrasamuel645@gmail.com>
2023-07-26 21:20:36 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      ReadexPro: ['Readex Pro'],
+      serif: ['DM Serif Display']
+    }
+  },
+  plugins: [],
+}
+
